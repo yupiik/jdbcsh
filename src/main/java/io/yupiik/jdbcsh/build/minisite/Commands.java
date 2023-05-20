b367ec80273391a7074ac26636d73a6cb857a9d3
Romain Manni-Bucau <rmannibucau@gmail.com>
2023-05-20 22:47:52 +0200

    setting up project for release (first pass) + creating a basic minisite (example and usage to add)

---
+++ src/main/java/io/yupiik/jdbcsh/build/minisite/Commands.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2023 - Yupiik SAS - https://www.yupiik.com
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package io.yupiik.jdbcsh.build.minisite;
+
+import io.yupiik.fusion.cli.internal.CliCommand;
+import io.yupiik.fusion.framework.api.ConfiguringContainer;
+import io.yupiik.fusion.framework.api.Instance;
+import io.yupiik.fusion.framework.api.RuntimeContainer;
+import io.yupiik.fusion.framework.api.container.FusionListener;
+import io.yupiik.fusion.framework.api.container.FusionModule;
+import io.yupiik.fusion.framework.api.container.bean.BaseBean;
+import io.yupiik.fusion.framework.api.main.Args;
+import io.yupiik.fusion.framework.api.scope.DefaultScoped;
+import io.yupiik.jdbcsh.io.StdIO;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiPredicate;
+
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.joining;
+
+public class Commands implements Runnable {
+    private final Path sourceBase;
+
+    public Commands(final Path sourceBase) {
+        this.sourceBase = sourceBase;
+    }
+
+    @Override
+    public void run() {
+        try (final var container = ConfiguringContainer.of()
+                .register(new BaseBean<Args>(Args.class, DefaultScoped.class, 1000, Map.of()) {
+                    @Override
+                    public Args create(final RuntimeContainer container, final List<Instance<?>> dependents) {
+                        return new Args(List.of());
+                    }
+                })
+                .register(new BaseBean<StdIO>(StdIO.class, DefaultScoped.class, 1000, Map.of()) {
+                    @Override
+                    public StdIO create(final RuntimeContainer container, final List<Instance<?>> dependents) {
+                        return new StdIO(new PrintStream(new ByteArrayOutputStream()), new PrintStream(new ByteArrayOutputStream()), new ByteArrayInputStream(new byte[0]));
+                    }
+                })
+                .register(new FusionModule() { // skip CLI since we don't want to launch anything
+                    @Override
+                    public int priority() { // before others to have filters active
+                        return 0;
+                    }
+
+                    @Override
+                    public BiPredicate<RuntimeContainer, FusionListener<?>> listenerFilter() {
+                        return (c, l) -> false;
+                    }
+                })
+                .start();
+             final var executor = container.lookups(CliCommand.class, i -> i.stream().map(Instance::instance).toList())) {
+            final var doc = "= Commands\n" +
+                    ":minisite-index: 300\n" +
+                    ":minisite-index-title: Commands\n" +
+                    ":minisite-index-description: Command list.\n" +
+                    ":minisite-index-icon: terminal\n" +
+                    "\n" +
+                    executor.instance().stream()
+                            .sorted(comparing(CliCommand::name))
+                            .map(c -> (CliCommand<Runnable>) c)
+                            .map(it -> "== " + it.name() + "\n" +
+                                    "\n" +
+                                    it.description() + "\n" +
+                                    "\n" +
+                                    "=== Parameters\n" +
+                                    "\n" +
+                                    (it.parameters().isEmpty() ?
+                                            ("statement".equals(it.name()) && it.parameters().isEmpty() ? "The SQL statement.\n" : "No parameter.\n") :
+                                            it.parameters().stream()
+                                                    .sorted(comparing(CliCommand.Parameter::cliName))
+                                                    .map(p -> "* `" + p.cliName().replace("-.", "--") + "`: " + p.description())
+                                                    .collect(joining("\n", "", "\n")) +
+                                                    "\n"))
+                            .collect(joining("\n\n"));
+            // todo: generate one file per command and just an index?
+            Files.writeString(Files.createDirectories(sourceBase.resolve("content/generated/commands")).resolve("index.adoc"), doc);
+        } catch (final IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+}
---
+++ src/main/java/io/yupiik/jdbcsh/build/minisite/Configuration.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2023 - Yupiik SAS - https://www.yupiik.com
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package io.yupiik.jdbcsh.build.minisite;
+
+import io.yupiik.fusion.json.internal.JsonMapperImpl;
+import io.yupiik.fusion.json.pretty.PrettyJsonMapper;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import static java.util.Optional.empty;
+import static java.util.stream.Collectors.toMap;
+
+public class Configuration implements Runnable {
+    private final Path sourceBase;
+    private final Path outputBase;
+
+    public Configuration(final Path sourceBase, final Path outputBase) {
+        this.sourceBase = sourceBase;
+        this.outputBase = outputBase;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public void run() {
+        final var schema = sourceBase.getParent().getParent().getParent().resolve("target/classes/META-INF/fusion/json/schemas.json");
+        final var out = sourceBase.resolve("content/_partials/generated/configuration.adoc");
+        try (final var jsonMapper = new JsonMapperImpl(List.of(), k -> empty())) {
+            Files.createDirectories(out.getParent());
+            final var schemaContent = Files.readString(schema);
+            final var schemaInstance = jsonMapper.fromString(Object.class, schemaContent);
+            final var schemas = (Map<String, Object>) ((Map<String, Object>) schemaInstance).get("schemas");
+            final var resolvedSchema = new PrettyJsonMapper(jsonMapper)
+                    .toString(resolveRefs(schemas, schemas.get(io.yupiik.jdbcsh.configuration.Configuration.class.getName())));
+
+            Files.writeString( // doc usage
+                    out.getParent().resolve("configuration.schema.json"),
+                    resolvedSchema);
+            Files.writeString( // ref in json files
+                    Files.createDirectories(outputBase).resolve("configuration.schema.json"),
+                    resolvedSchema);
+
+            // clean up .adoc which is too technical out of the box
+            final var adoc = sourceBase.resolve("content/_partials/generated/configuration.json.adoc");
+            Files.writeString(adoc, Files.readString(adoc)
+                    // drop qualified name from titles
+                    .replace("= io.yupiik.jdbcsh.configuration.", "= ")
+                    .replace("<<io.yupiik.jdbcsh.configuration.", "<<conf_")
+                    .replace("[#io.yupiik.jdbcsh.configuration.", "[#conf_"));
+        } catch (final IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private Object resolveRefs(final Map<String, Object> schemas, final Object schema) {
+        if (!(schema instanceof Map<?, ?> map)) {
+            return schema;
+        }
+
+        final var ref = map.get("$ref");
+        if (ref instanceof String s && s.startsWith("#/schemas/")) {
+            final var resolved = schemas.get(s.substring("#/schemas/".length()));
+            if (resolved != null) {
+                final Map<String, Object> out = new TreeMap<>((Map<String, ?>) map);
+                out.remove("$ref");
+                out.putAll((Map<String, Object>) resolveRefs(schemas, resolved));
+                return out;
+            }
+        }
+
+        final var items = map.get("items");
+        if (items instanceof Map<?, ?> itemsAsMap) {
+            final var resolved = resolveRefs(schemas, itemsAsMap);
+            if (resolved != itemsAsMap) {
+                final Map<String, Object> out = new TreeMap<>((Map<String, ?>) map);
+                out.put("items", resolved);
+                return out;
+            }
+        }
+
+        final var properties = map.get("properties");
+        if (properties instanceof Map<?, ?> propertiesAsMap) {
+            final Map<String, Object> out = (Map<String, Object>) new TreeMap<>(map);
+            final Map<String, Object> newProps = ((Map<String, Object>) propertiesAsMap)
+                    .entrySet().stream()
+                    .collect(toMap(Map.Entry::getKey, e -> resolveRefs(schemas, e.getValue()), (a, b) -> a, TreeMap::new));
+            out.put("properties", newProps);
+            return out;
+        }
+
+        return schema;
+    }
+}
